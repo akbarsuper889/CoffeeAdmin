52cf92f8e2ad067ff085d69761f2ffce3e67289e
Akbarsuper889 <sempakgaring889@gmail.com>
2021-01-15 19:49:16 +0700

    Add project files.

---
+++ Models/Coffee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoffeeAdmin.Models
+{
+    public partial class Coffee
+    {
+        public int Id { get; set; }
+        public string NamaKopi { get; set; }
+        public string AsalKopi { get; set; }
+        public string JenisKopi { get; set; }
+        public string DeskripsiKopi { get; set; }
+    }
+}
---
+++ Models/CoffeeContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoffeeAdmin.Models
+{
+    public partial class CoffeeContext : DbContext
+    {
+        public CoffeeContext()
+        {
+        }
+
+        public CoffeeContext(DbContextOptions<CoffeeContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Coffee> Coffee { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coffee>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.AsalKopi)
+                    .HasColumnName("Asal_Kopi")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.DeskripsiKopi)
+                    .HasColumnName("Deskripsi_Kopi")
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.JenisKopi)
+                    .HasColumnName("Jenis_Kopi")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NamaKopi)
+                    .HasColumnName("Nama_Kopi")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
